84c7030acc3bbfa51e58039f0d1a24adb4557bd8
unknown <matheus.fcarvalho2001@gmail.com>
2023-07-10 11:12:23 -0300

    lilFeat: def arrPrinter, checkSizeLockArr from functions.py

---
+++ index.py
@@ -0,0 +1,16 @@
+from functions import arrPrinter, checkSizeLockArr
+from databases import karms
+
+
+# facesNames = rostosNomes = lilDbFace = firstDbPy = bigStringToObject(bigStringWithTabEnter)
+# faces = facesArr = arrFace = oldEmotis = bigStringOrBigObjectToSimpleArray(firstDbPy)
+
+# testes sendo feito!
+checkSizeLockArr(karms)
+
+#animaçãozinhaaa, podemos acelerar os frames por segundo com o segundo parametro
+# arrPrinter(karms, 13)
+
+
+# a velocidade padrão foi calculada de um modo engenhoso pois eu gosto do numero 5
+arrPrinter(karms)
